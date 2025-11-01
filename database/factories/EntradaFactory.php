f63e9b8f3a2b3b80be5491a60e48b3d71d03dec6
FlavysonFelipe314 <flavysonfelipe314@gmail.com>
2025-11-01 19:39:19 -0300

    feat(db): Implementação de estrutura de dados da aplicação

---
+++ database/factories/EntradaFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Conta;
+use App\Models\InstitutionType;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Entrada>
+ */
+class EntradaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'conta_id' => Conta::all()->random()->id,
+            'title' => fake()->words(4, true),
+            'category' => fake()->words(1, true),
+            'payment_method' => fake()->randomElement(['P','C','D','B','K' ])  ,
+            'amount' => fake()->randomFloat(2, 50, 500),
+        ];
+    }
+}
---
+++ database/migrations/2025_11_01_214608_create_saidas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('saidas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
+            $table->foreignId('conta_id')
+            ->constrained('contas')
+            ->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
+            $table->string('title', 255);
+            $table->string('category', 255);
+            $table->string('payment_method', 255); //pix, cartão, boleto...
+            $table->decimal('amount', 10, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('saidas');
+    }
+};
