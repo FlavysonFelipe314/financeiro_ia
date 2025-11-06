2fa882a9049f072bc91dde6fb937b35935021842
FlavysonFelipe314 <flavysonfelipe314@gmail.com>
2025-11-05 21:11:12 -0300

    Feat: Aplicando Logica e rotas de entrada

--- app/Http/Requests/StoreEntradaRequest.php
+++ app/Http/Requests/StoreEntradaRequest.php
@@ -11,7 +11,7 @@ class StoreEntradaRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class StoreEntradaRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'conta_id' => ['required'],
+            'title' => ['required','max:255'],
+            'category' => ['required','max:255'],
+            'payment_method' => ['required'],
+            'amount' => ['required'],
         ];
     }
 }
